2104460cfdf663229e24fd2e4baa65c218485419
Douglas Blank <doug.blank@gmail.com>
2018-12-05 09:18:30 -0500

    Release 0.9.2

--- aiml/constants.py
+++ aiml/constants.py
@@ -4,7 +4,7 @@ Some general-purpose constants, including Python3/Python2 discrimination
 import sys
 
 # Package version
-VERSION = '0.9.1'
+VERSION = '0.9.2'
 
 # Python 2/3 compatibility
 PY3 = sys.version_info[0] == 3
